45a8f9ae90843e00f184d95d0a00717121dd2823
Ari P <ariteknologi@gmail.com>
2025-01-10 00:48:18 +0700

    fix prettier

--- src/routes/RoleRoute.ts
+++ src/routes/RoleRoute.ts
@@ -1,25 +1,24 @@
 import { Elysia } from 'elysia';
-import { 
-  find as findRole,
-  get as getRoles,
-  store as storeRoles,
-  update as updateRoles,
-  destroy as destroyRoles
+import {
+	find as findRole,
+	get as getRoles,
+	store as storeRoles,
+	update as updateRoles,
+	destroy as destroyRoles
 } from '../controllers/RoleController';
 import {
-  storeValidation as storeRoleValidation,
-  updateValidation as updateRoleValidation,
-  destroyValidation as destroyRoleValidation
+	storeValidation as storeRoleValidation,
+	updateValidation as updateRoleValidation,
+	destroyValidation as destroyRoleValidation
 } from '../validators/RoleValidator';
 
-const app = new Elysia()
-  .group('/roles', (app) =>
-    app
-      .get('/', getRoles)
-      .get('/:id', findRole)
-      .post('/', storeRoles, storeRoleValidation)
-      .put('/', updateRoles, updateRoleValidation)
-      .delete('/', destroyRoles, destroyRoleValidation)
-  );
+const app = new Elysia().group('/roles', (app) =>
+	app
+		.get('/', getRoles)
+		.get('/:id', findRole)
+		.post('/', storeRoles, storeRoleValidation)
+		.put('/', updateRoles, updateRoleValidation)
+		.delete('/', destroyRoles, destroyRoleValidation)
+);
 
-export default app
+export default app;
