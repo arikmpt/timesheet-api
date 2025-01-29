7428e402a89d418f222364174974a098f6e88985
Ari P <ariteknologi@gmail.com>
2025-01-30 00:35:32 +0700

    add user to vendor api (#5)

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,5 +1,5 @@
 import { jwtPlugin, verifyToken } from '@plugins/security';
-import { changePasswordSchema, profileResponseSchema, updateProfileSchema } from '@schemas/user';
+import { assignToVendorSchema, changePasswordSchema, profileResponseSchema, updateProfileSchema } from '@schemas/user';
 import Service from '@services/UserService';
 import { Elysia } from 'elysia';
 
@@ -13,6 +13,11 @@ export default new Elysia({
 })
   .use(jwtPlugin)
   .use(verifyToken)
+  .derive(({ userToken }) => ({
+    service: new Service(userToken)
+  }))
   .get('/profile', ({ userToken }) => service.profile(userToken?.id), profileResponseSchema)
   .put('/profile', ({ body, userToken }) => service.updateProfile(body, userToken?.id), updateProfileSchema)
-  .put('/change-password', ({ body, userToken }) => service.changePassword(body, userToken?.id), changePasswordSchema);
+  .put('/change-password', ({ body, userToken }) => service.changePassword(body, userToken?.id), changePasswordSchema)
+  .put('/assign-to-vendor', ({ body }) => service.assignUserToVendor(body), assignToVendorSchema)
+  .put('/unassign-from-vendor', ({ body }) => service.unassignUserFromVendor(body), assignToVendorSchema);
